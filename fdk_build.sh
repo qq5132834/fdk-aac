ac819719ce6e050e8e7794208fec895060e9fc7e
qq5132834 <513283439@qq.com>
2024-03-30 16:10:17 +0800

    添加 fdk_build.sh 文件

---
+++ fdk_build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+NDK=/home/ubuntu/Desktop/ndk/android-ndk-r22b
+echo "android-ndk-r22b 的安装路径:  $NDK"
+
+HOST_TAG=linux-x86_64
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_TAG
+ANDROID_LIB_PATH="$(pwd)/android"
+
+API=21
+./autogen.sh
+function build_android_arm
+{
+ echo "build for android $CPU"
+ ./configure \
+ --host=$HOST \
+ --disable-shared \
+ --enable-static \
+ --prefix="$ANDROID_LIB_PATH/$CPU"
+
+ make clean
+ make -j8
+ make install
+ echo "building for android $CPU completed"
+}
+
+CPU=armv8-a
+HOST=aarch64-linux-android
+
+
+export AR=$TOOLCHAIN/bin/aarch64-linux-android-ar
+export AS=$TOOLCHAIN/bin/aarch64-linux-android-as
+export LD=$TOOLCHAIN/bin/aarch64-linux-android-ld
+export RANLIB=$TOOLCHAIN/bin/aarch64-linux-android-ranlib
+export STRIP=$TOOLCHAIN/bin/aarch64-linux-android-strip
+export CC=$TOOLCHAIN/bin/aarch64-linux-android$API-clang
+export CXX=$TOOLCHAIN/bin/aarch64-linux-android$API-clang++
+export CFLAGS="-Os -fpic $OPTIMIZE_CFLAGS"
+export CPPFLAGS="-Os -fpic $OPTIMIZE_CFLAGS"
+
+build_android_arm
+
+
+
+
